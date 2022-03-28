656ef14271e4ec7bec709082808de261cfc08f37
hyh <1360895771@qq.com>
2022-03-28 16:09:36 +0800

    master

--- 4.专选课/作业/bar_code.py
+++ 4.专选课/作业/bar_code.py
@@ -35,5 +35,5 @@ def draw_bar_code(stu_number):
     goto(-10,-10)
     write(num,move = True, align="left", font=('微软雅黑',5,'bold'))
 
-draw_bar_code(42027150)
+draw_bar_code(420271500000)
 exitonclick()
